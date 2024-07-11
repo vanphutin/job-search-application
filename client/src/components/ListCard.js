613a31579e50f5c48459c867d6edc637de32f58f
vanphutin2004 <vanphutin.contact@gmail.com>
2024-07-11 15:42:41 +0700

    list job

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -1,39 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
-import JobDetails from "./JobDetails";
+import { format } from "date-fns"; // Thư viện date-fns (tùy chọn)
 
 const ListCard = () => {
-  const job = [
-    {
-      id: 1,
-      date: "20 May, 2024",
-      companyName: "Google",
-      titleJob: "Full Stack Developer",
-      logoCompany:
-        "https://static.vecteezy.com/system/resources/previews/022/613/027/non_2x/google-icon-logo-symbol-free-png.png",
-      salary: "260",
-    },
-    {
-      id: 2,
-      date: "24 May, 2024",
-      companyName: "MGM technology",
-      titleJob: "Full Stack Developer",
-      logoCompany:
-        "https://reviewcongty.com/images/companies/mgm-technology-partners-vietnam-logo.png",
-      salary: "240",
-    },
-  ];
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8081/api/v1/jobs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        setJobs(data.jobs);
+      });
+  }, []);
+
   return (
     <div className="row" style={{ margin: "20px", gap: "10px" }}>
-      {job && job.length > 0
-        ? job.map((items, index) => (
+      {jobs && jobs.length > 0
+        ? jobs.map((items, index) => (
             <Card
-              date={items.date}
-              companyName={items.companyName}
-              titleJob={items.titleJob}
-              logoCompany={items.logoCompany}
+              date={items.createdAt.toString().split("-").slice(0, 2).join("-")}
+              companyName={items.company}
+              titleJob={items.title}
+              logoCompany={items.avatar}
               salary={items.salary}
-              id={items.id}
+              id={items.idJob}
               key={index}
             />
           ))
