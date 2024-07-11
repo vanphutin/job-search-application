4d36bb1cd941e736f52f75ffb7637edc8f1ec138
vanphutin2004 <vanphutin.contact@gmail.com>
2024-07-11 12:10:21 +0700

    create user

---
+++ client/src/components/Admin/ModalCreateUser.js
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+
+function ModalCreateUser({ show, handleClose }) {
+  const [lastname, setLastName] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [username, setUserName] = useState("");
+  const [avata, setImage] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmitForm = async (e) => {
+    e.preventDefault();
+    const user = {
+      lastname,
+      firstname,
+      email,
+      username,
+      password,
+      avata,
+    };
+    console.log("user", user);
+    try {
+      const response = await fetch("http://localhost:8081/api/v1/create", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      const result = await response.json();
+      handleClose();
+    } catch (error) {
+      console.log("Error when creating user", error);
+    }
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>Create User</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmitForm}>
+          <Form.Group controlId="exampleInputLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter LastName"
+              value={lastname}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleInputFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter FirstName"
+              value={firstname}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleInputEmail1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleInputUserName">
+            <Form.Label>User Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter user name"
+              value={username}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleInputPassword1">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="exampleInputImage">
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Image"
+              value={avata}
+              onChange={(e) => setImage(e.target.value)}
+            />
+          </Form.Group>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Create
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default ModalCreateUser;
